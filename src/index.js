06c4fa4ce3ee8f36f0859b5ca8e5f5a2f5b47255
Adrian Prananda <prananda0203@gmail.com>
2018-07-12 23:49:52 -0700

    adding flow typed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'connected-react-router'
-import store, { history } from './store'
-import App from './containers/app'
+// @flow
 
-import 'sanitize.css/sanitize.css'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import store, { history } from './store';
+import App from './containers/app';
+
 import './index.css'
 
-const target = document.querySelector('#root')
+const target = document.querySelector('#root');
+
+if (!target) {
+  throw new Error('Invalid root type');
+}
 
 render(
   <Provider store={store}>
@@ -19,4 +24,5 @@ render(
     </ConnectedRouter>
   </Provider>,
   target
-)
+);
+
--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
+// @flow
+
 import { createStore, applyMiddleware, compose } from 'redux'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
 import thunk from 'redux-thunk'
-import createHistory from 'history/createBrowserHistory'
+import createHistory from 'history/createBrowserHistory';
 import rootReducer from './modules'
 
 export const history = createHistory()
